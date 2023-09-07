c50149e064475efb159ae16e87b7226497b2002a
vkmtx <110697534+vkmtx@users.noreply.github.com>
2023-09-06 22:34:00 -0300

    Update _banner.sh

--- utils/_banner.sh
+++ utils/_banner.sh
@@ -19,30 +19,16 @@ print_banner() {
 
   clear
 
-  printf "\n\n"
-
-  printf "${GREEN}";
-  printf "                                                     ▄▄█▀▀▀▀▀▀▀█▄▄  \n";
-  printf "                                                   ${GREEN}▄█▀${NC}   ${WHITE}▄▄${NC}      ${GREEN}▀█▄\n";
-  printf "                                                   ${GREEN}█${NC}    ${WHITE}███${NC}         ${GREEN}█\n";
-  printf "                                                   ${GREEN}█${NC}    ${WHITE}██▄         ${GREEN}█${NC}\n";
-  printf "                                                   ${GREEN}█${NC}     ${WHITE}▀██▄${NC} ${WHITE}██${NC}    ${GREEN}█\n";
-  printf "                                                   ${GREEN}█${NC}       ${WHITE}▀███▀${NC}    ${GREEN}█\n";
-  printf "                                                   ${GREEN}▀█▄           ▄█▀\n";
-  printf "                                                    ▄█    ▄▄▄▄█▀▀  \n";
-  printf "                                                    █  ▄█▀        \n";
-  printf "                                                    ▀▀▀▀          \n";
-  printf "${NC}";
-
-  printf "\n"
-
-printf "${GREEN}";  
-printf "██████╗░██╗░░░░░░██╗░░░░░░░██╗\n";
-printf "██╔══██╗██║░░░░░░██║░░██╗░░██║\n";
-printf "██████╔╝██║░░░░░░╚██╗████╗██╔╝\n";
-printf "██╔═══╝░██║░░░░░░░████╔═████║░\n";
-printf "██║░░░░░███████╗░░╚██╔╝░╚██╔╝░\n";
-printf "╚═╝░░░░░╚══════╝░░░╚═╝░░░╚═╝░░\n";
+printf "${WHITE}";
+printf "███▄    █  ▒█████  ▄▄▄█████▓ ██▓ ▒█████    \n";
+printf "██ ▀█   █ ▒██▒  ██▒▓  ██▒ ▓▒▓██▒▒██▒  ██▒  \n";
+printf "▓██  ▀█ ██▒▒██░  ██▒▒ ▓██░ ▒░▒██▒▒██░  ██▒ \n";
+printf "▓██▒  ▐▌██▒▒██   ██░░ ▓██▓ ░ ░██░▒██   ██░ \n";
+printf "▒██░   ▓██░░ ████▓▒░  ▒██▒ ░ ░██░░ ████▓▒░ \n";
+printf "░ ▒░   ▒ ▒ ░ ▒░▒░▒░   ▒ ░░   ░▓  ░ ▒░▒░▒░  \n";
+printf "░ ░░   ░ ▒░  ░ ▒ ▒░     ░     ▒ ░  ░ ▒ ▒░  \n";
+printf "  ░   ░ ░ ░ ░ ░ ▒    ░       ▒ ░░ ░ ░ ▒    \n";
+printf "        ░     ░ ░            ░      ░ ░    \n";
 printf "${NC}";
   
   
